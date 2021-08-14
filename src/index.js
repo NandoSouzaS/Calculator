e88c6963c2345ef88ddb71af2fbb036761d876be
Fernando Souza <fernando.souza.ctt@gmail.com>
2021-08-14 12:22:36 -0300

    first version

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Calculator from './main/Calculator';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <div>
+    <h1>Calculator</h1>
+    <Calculator />
+    <footer>
+     Developed with <div className='heart'>&#10084;</div> by: <span className='devName'>Fernando Souza</span>
+    </footer>
+  </div>
+  , document.getElementById('root')
+);
+reportWebVitals();
