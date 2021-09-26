74ae76c0b458cded8a8ef45123736634ed5c26ff
José Fernando de Andrade Souza <fernando.souza.ctt@gmail.com>
2021-09-26 12:07:01 -0300

    minor

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Calculator from './../src/main/Calculator';
-
-test('renders learn react link', () => {
-  render(<Calculator />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
