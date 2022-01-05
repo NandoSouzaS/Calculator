7caf446257f0c5bbba6bfbb07d787403a623f418
José Fernando de Andrade Souza <fernando.souza.ctt@gmail.com>
2022-01-05 16:29:22 -0300

    prettier

--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -1,6 +1,4 @@
-import React from 'react'
-import './Display.css'
+import React from "react";
+import "./Display.css";
 
-// eslint-disable-next-line import/no-anonymous-default-export
-export default props =>
-    <div className="display">{props.value}</div>
+export default (props) => <div className="display">{props.value}</div>;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Calculator from './main/Calculator';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Calculator from "./main/Calculator";
 ReactDOM.render(
   <div>
     <h1>Calculator</h1>
     <Calculator />
     <footer>
-     Developed with <div className='heart'>&#10084;</div> by: <span className='devName'>Fernando Souza</span>
+      Developed with <div className="heart">&#10084;</div> by:{" "}
+      <span className="devName">Fernando Souza</span>
     </footer>
-  </div>
-  , document.getElementById('root')
+  </div>,
+  document.getElementById("root")
 );
-reportWebVitals();
